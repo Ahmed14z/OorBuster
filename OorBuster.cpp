ac01d8a28d9a30b3ab723f93f36757f4bccb165c
Ahmed Mansy <93006780+Ahmed14z@users.noreply.github.com>
2024-12-05 18:12:05 +0200

    Update OorBuster.cpp

--- OorBuster.cpp
+++ OorBuster.cpp
@@ -10,15 +10,14 @@
 const BYTE VCP_OSD_LANGUAGE = 0xCC;
 const BYTE VCP_PICTURE_MODE = 0xDC;
 const BYTE VCP_SCAN_MODE = 0xDA;
-
 const BYTE VCP_AMA = 0xF0;
 
 enum
 {
-	WM_APP_RELOAD = WM_APP + 1,
-	WM_APP_EXIT,
-	WM_APP_NEXT_MODE,
-	WM_APP_PREV_MODE
+    WM_APP_RELOAD = WM_APP + 1,
+    WM_APP_EXIT,
+    WM_APP_NEXT_MODE,
+    WM_APP_PREV_MODE
 };
 
 DWORD CachedOsdLanguage = 2;
@@ -29,42 +28,42 @@ DWORD OorDelay = 2000;
 DWORD PicDelay = 500;
 DWORD AmaDelay = 250;
 DWORD WakeDelay = 3000;
-DWORD CachedScanMode = 0;  // Default to normal mode.
+DWORD CachedScanMode = 0; // Default to normal mode.
 
 NOTIFYICONDATA TrayIcon;
 HANDLE Monitor;
 
 int GetRefreshRate()
 {
-	DEVMODE dm;
-	dm.dmSize = sizeof(DEVMODE);
-	EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &dm);
+    DEVMODE dm;
+    dm.dmSize = sizeof(DEVMODE);
+    EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &dm);
 
-	return dm.dmDisplayFrequency;
+    return dm.dmDisplayFrequency;
 }
 
 HANDLE GetPhysicalMonitor()
 {
-	HMONITOR monitor = MonitorFromWindow(GetActiveWindow(), MONITOR_DEFAULTTOPRIMARY);
-	LPPHYSICAL_MONITOR monitors = (LPPHYSICAL_MONITOR)malloc(sizeof(PHYSICAL_MONITOR));
-	HANDLE pm = nullptr;
+    HMONITOR monitor = MonitorFromWindow(GetActiveWindow(), MONITOR_DEFAULTTOPRIMARY);
+    LPPHYSICAL_MONITOR monitors = (LPPHYSICAL_MONITOR)malloc(sizeof(PHYSICAL_MONITOR));
+    HANDLE pm = nullptr;
 
-	if (monitors && GetPhysicalMonitorsFromHMONITOR(monitor, 1, monitors))
-		pm = monitors[0].hPhysicalMonitor;
+    if (monitors && GetPhysicalMonitorsFromHMONITOR(monitor, 1, monitors))
+        pm = monitors[0].hPhysicalMonitor;
 
-	free(monitors);
+    free(monitors);
 
-	return pm;
+    return pm;
 }
 
 void CachePhysicalMonitor()
 {
-	Monitor = GetPhysicalMonitor();
+    Monitor = GetPhysicalMonitor();
 }
 
 void CacheVcpValues()
 {
-	GetVCPFeatureAndVCPFeatureReply(Monitor, VCP_OSD_LANGUAGE, NULL, &CachedOsdLanguage, NULL);
+    GetVCPFeatureAndVCPFeatureReply(Monitor, VCP_OSD_LANGUAGE, NULL, &CachedOsdLanguage, NULL);
     GetVCPFeatureAndVCPFeatureReply(Monitor, VCP_PICTURE_MODE, NULL, &CachedPictureMode, NULL);
     GetVCPFeatureAndVCPFeatureReply(Monitor, VCP_AMA, NULL, &CachedAma, NULL);
     GetVCPFeatureAndVCPFeatureReply(Monitor, VCP_SCAN_MODE, NULL, &CachedScanMode, NULL);
@@ -72,240 +71,253 @@ void CacheVcpValues()
 
 void ReadLaunchParams()
 {
-	int argCount;
-	LPWSTR* args = CommandLineToArgvW(GetCommandLine(), &argCount);
+    int argCount;
+    LPWSTR* args = CommandLineToArgvW(GetCommandLine(), &argCount);
 
-	if (args && argCount > 1)
-	{
-		OorDelay = (DWORD)wcstod(args[1], nullptr);
-		PicDelay = (DWORD)wcstod(args[2], nullptr);
-		AmaDelay = (DWORD)wcstod(args[3], nullptr);
-		WakeDelay = (DWORD)wcstod(args[4], nullptr);
-
-	}
+    if (args && argCount > 1)
+    {
+        OorDelay = (DWORD)wcstod(args[1], nullptr);
+        PicDelay = (DWORD)wcstod(args[2], nullptr);
+        AmaDelay = (DWORD)wcstod(args[3], nullptr);
+        WakeDelay = (DWORD)wcstod(args[4], nullptr);
+    }
 }
 
-inline void FixOor() {SetVCPFeature(Monitor, VCP_OSD_LANGUAGE, CachedOsdLanguage);}
-inline void FixPic() {SetVCPFeature(Monitor, VCP_PICTURE_MODE, CachedPictureMode);}
-inline void FixAma() {SetVCPFeature(Monitor, VCP_AMA, CachedAma);}
+inline void FixOor() { SetVCPFeature(Monitor, VCP_OSD_LANGUAGE, CachedOsdLanguage); }
+inline void FixPic() { SetVCPFeature(Monitor, VCP_PICTURE_MODE, CachedPictureMode); }
+inline void FixAma() { SetVCPFeature(Monitor, VCP_AMA, CachedAma); }
 inline void FixScanMode()
 {
-    // Set Scan Mode to Underscan (value depends on your monitor, typically 2 or 3 for underscan).
-    SetVCPFeature(Monitor, VCP_SCAN_MODE, 1); 
+    // Set Scan Mode to Underscan (typically 3 for most monitors).
+    SetVCPFeature(Monitor, VCP_SCAN_MODE, 3);
+}
+
+void SetRefreshRate(int refreshRate)
+{
+    DEVMODE dm;
+    ZeroMemory(&dm, sizeof(dm));
+    dm.dmSize = sizeof(dm);
+
+    if (EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &dm))
+    {
+        dm.dmDisplayFrequency = refreshRate;
+        dm.dmFields = DM_DISPLAYFREQUENCY;
+
+        LONG result = ChangeDisplaySettings(&dm, CDS_UPDATEREGISTRY);
+
+        if (result == DISP_CHANGE_SUCCESSFUL)
+        {
+            OutputDebugString(L"[INFO] Refresh rate set to 144Hz\n");
+        }
+        else
+        {
+            OutputDebugString(L"[ERROR] Failed to set refresh rate\n");
+        }
+    }
 }
 
 void NextMode()
 {
-	CachePhysicalMonitor();
-	CacheVcpValues();
-	CachedPictureMode++;
-	FixPic();
-	FixScanMode(); // Ensure scan mode is set appropriately.
-
+    CachePhysicalMonitor();
+    CacheVcpValues();
+    CachedPictureMode++;
+    FixPic();
+    FixScanMode();
 }
 
 void PrevMode()
 {
-	CachePhysicalMonitor();
-	CacheVcpValues();
-	CachedPictureMode--;
-	FixPic();
-	FixScanMode(); // Ensure scan mode is set appropriately.
-
+    CachePhysicalMonitor();
+    CacheVcpValues();
+    CachedPictureMode--;
+    FixPic();
+    FixScanMode();
 }
 
 void ApplyVcpValues(bool wake = false)
 {
-	if (GetRefreshRate() <= 144)
+    if (GetRefreshRate() <= 144)
         return;
 
     if (wake)
         Sleep(WakeDelay);
 
-    Sleep(OorDelay); FixOor();
-    Sleep(PicDelay); FixPic();
-    Sleep(AmaDelay); FixAma();
-    Sleep(250); FixScanMode(); // Add this line to set scan mode after other settings.
+    Sleep(OorDelay);
+    FixOor();
+    Sleep(PicDelay);
+    FixPic();
+    Sleep(AmaDelay);
+    FixAma();
+    Sleep(250);
+    FixScanMode();
 }
 
 void ShowTrayMenu(HWND wnd)
 {
-	POINT pos;
-	GetCursorPos(&pos);
+    POINT pos;
+    GetCursorPos(&pos);
 
-	HMENU menu = CreatePopupMenu();
-	InsertMenu(menu, -1, MF_BYPOSITION, WM_APP_RELOAD, L"Reload");
-	InsertMenu(menu, -1, MF_BYPOSITION, WM_APP_NEXT_MODE, L"Next Mode");
-	InsertMenu(menu, -1, MF_BYPOSITION, WM_APP_PREV_MODE, L"Prev Mode");
-	InsertMenu(menu, -1, MF_BYPOSITION, WM_APP_EXIT, L"Exit");
-	SetForegroundWindow(wnd);
-	TrackPopupMenu(menu, TPM_BOTTOMALIGN, pos.x, pos.y, 0, wnd, NULL);
-	DestroyMenu(menu);
+    HMENU menu = CreatePopupMenu();
+    InsertMenu(menu, -1, MF_BYPOSITION, WM_APP_RELOAD, L"Reload");
+    InsertMenu(menu, -1, MF_BYPOSITION, WM_APP_NEXT_MODE, L"Next Mode");
+    InsertMenu(menu, -1, MF_BYPOSITION, WM_APP_PREV_MODE, L"Prev Mode");
+    InsertMenu(menu, -1, MF_BYPOSITION, WM_APP_EXIT, L"Exit");
+    SetForegroundWindow(wnd);
+    TrackPopupMenu(menu, TPM_BOTTOMALIGN, pos.x, pos.y, 0, wnd, NULL);
+    DestroyMenu(menu);
 }
 
 LRESULT CALLBACK WindowProc(_In_ HWND wnd, _In_ UINT msg, _In_ WPARAM wParam, _In_ LPARAM lParam)
 {
-	switch (msg)
-	{
-	case WM_DISPLAYCHANGE:
-		CachePhysicalMonitor();
-		ApplyVcpValues();
+    switch (msg)
+    {
+    case WM_DISPLAYCHANGE:
+        CachePhysicalMonitor();
+        ApplyVcpValues();
+        break;
+    case WM_POWERBROADCAST:
+        if (wParam == PBT_POWERSETTINGCHANGE)
+        {
+            const DWORD DISPLAY_ON = 0x1;
+            POWERBROADCAST_SETTING* pbs = (POWERBROADCAST_SETTING*)lParam;
+            DWORD status = *(DWORD*)(pbs->Data);
 
-		break;
-	case WM_POWERBROADCAST:
-		if (wParam == PBT_POWERSETTINGCHANGE)
-		{
-			const DWORD DISPLAY_ON = 0x1;
-
-			POWERBROADCAST_SETTING* pbs = (POWERBROADCAST_SETTING *)lParam;
-			DWORD status = *(DWORD*)(pbs->Data);
-
-			if (status == DISPLAY_ON)
-			{
-				CachePhysicalMonitor();
-				ApplyVcpValues(true);
-			}
-		}
-
-		break;
-	case WM_APP:
-		switch (lParam)
-		{
-		case WM_RBUTTONDOWN:
-		case WM_CONTEXTMENU:
-			ShowTrayMenu(wnd);
-
-			break;
-		}
-
-		break;
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
-		case WM_APP_RELOAD:
-			CachePhysicalMonitor();
-			FixOor();
-			CacheVcpValues();
-
-			break;
-		case WM_APP_NEXT_MODE:
-			NextMode();
-			break;
-		case WM_APP_PREV_MODE:
-			PrevMode();
-			break;
-		case WM_APP_EXIT:
-			Shell_NotifyIcon(NIM_DELETE, &TrayIcon);
-			PostQuitMessage(0);
-
-			break;
-		}
-
-		break;
-	case WM_DESTROY:
-		Shell_NotifyIcon(NIM_DELETE, &TrayIcon);
-		PostQuitMessage(0);
-
-		break;
-	case WM_HOTKEY:
-		switch (wParam)
-		{
-		case 1000:
-			{
-				OutputDebugString(L"[DEBUG] HOTKEY!\n");
-				CachePhysicalMonitor();
-
-				DWORD origOorDelay = OorDelay;
-				OorDelay = 0;
-				ApplyVcpValues();
-				OorDelay = origOorDelay;
-			}
-
-			break;
-		}
-	}
-
-	return DefWindowProc(wnd, msg, wParam, lParam);
+            if (status == DISPLAY_ON)
+            {
+                CachePhysicalMonitor();
+                ApplyVcpValues(true);
+            }
+        }
+        break;
+    case WM_APP:
+        switch (lParam)
+        {
+        case WM_RBUTTONDOWN:
+        case WM_CONTEXTMENU:
+            ShowTrayMenu(wnd);
+            break;
+        }
+        break;
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case WM_APP_RELOAD:
+            CachePhysicalMonitor();
+            FixOor();
+            CacheVcpValues();
+            break;
+        case WM_APP_NEXT_MODE:
+            NextMode();
+            break;
+        case WM_APP_PREV_MODE:
+            PrevMode();
+            break;
+        case WM_APP_EXIT:
+            Shell_NotifyIcon(NIM_DELETE, &TrayIcon);
+            PostQuitMessage(0);
+            break;
+        }
+        break;
+    case WM_DESTROY:
+        Shell_NotifyIcon(NIM_DELETE, &TrayIcon);
+        PostQuitMessage(0);
+        break;
+    case WM_HOTKEY:
+        switch (wParam)
+        {
+        case 1000: // Alt + Home
+            OutputDebugString(L"[DEBUG] HOTKEY: Apply VCP Values\n");
+            CachePhysicalMonitor();
+            ApplyVcpValues();
+            break;
+        case 1001: // Alt + F8
+            OutputDebugString(L"[DEBUG] HOTKEY: Set Refresh Rate to 144Hz\n");
+            SetRefreshRate(144);
+            break;
+        }
+        break;
+    }
+    return DefWindowProc(wnd, msg, wParam, lParam);
 }
 
 HWND CreateMainWindow(HINSTANCE instance)
 {
-	WNDCLASS wc = { };
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = instance;
-	wc.lpszClassName = L"Main";
-	RegisterClass(&wc);
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = instance;
+    wc.lpszClassName = L"Main";
+    RegisterClass(&wc);
 
-	HWND wnd = CreateWindow(L"Main", L"OOR Buster", WS_OVERLAPPEDWINDOW & ~WS_SIZEBOX & ~WS_MAXIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, NULL, NULL, instance, NULL);
-
-	return wnd;
+    HWND wnd = CreateWindow(L"Main", L"OOR Buster", WS_OVERLAPPEDWINDOW & ~WS_SIZEBOX & ~WS_MAXIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, NULL, NULL, instance, NULL);
+    return wnd;
 }
 
 void CreateTrayIcon(HWND wnd, HICON icon)
 {
-	ZeroMemory(&TrayIcon, sizeof(NOTIFYICONDATA));
-	TrayIcon.cbSize = sizeof(NOTIFYICONDATA);
-	TrayIcon.uID = 1;
-	TrayIcon.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
-	TrayIcon.hIcon = icon;
-	TrayIcon.hWnd = wnd;
-	TrayIcon.uCallbackMessage = WM_APP;
-	wcscpy_s(TrayIcon.szTip, L"OOR Buster");
-	Shell_NotifyIcon(NIM_ADD, &TrayIcon);
+    ZeroMemory(&TrayIcon, sizeof(NOTIFYICONDATA));
+    TrayIcon.cbSize = sizeof(NOTIFYICONDATA);
+    TrayIcon.uID = 1;
+    TrayIcon.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    TrayIcon.hIcon = icon;
+    TrayIcon.hWnd = wnd;
+    TrayIcon.uCallbackMessage = WM_APP;
+    wcscpy_s(TrayIcon.szTip, L"OOR Buster");
+    Shell_NotifyIcon(NIM_ADD, &TrayIcon);
 }
 
 void KillOtherInstances()
 {
-	DWORD id = GetCurrentProcessId();
+    DWORD id = GetCurrentProcessId();
 
-	WCHAR path[MAX_PATH]; 
-	GetModuleFileName(NULL, path, MAX_PATH);
+    WCHAR path[MAX_PATH];
+    GetModuleFileName(NULL, path, MAX_PATH);
 
-	WCHAR file[_MAX_FNAME]; 
-	_wsplitpath_s(path, NULL, 0, NULL, 0, file, ARRAYSIZE(file), NULL, 0); 
-	wcscat_s(file, ARRAYSIZE(file), L".exe");
+    WCHAR file[_MAX_FNAME];
+    _wsplitpath_s(path, NULL, 0, NULL, 0, file, ARRAYSIZE(file), NULL, 0);
+    wcscat_s(file, ARRAYSIZE(file), L".exe");
 
-	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
 
-	PROCESSENTRY32 entry; 
-	entry.dwSize = sizeof(entry);
+    PROCESSENTRY32 entry;
+    entry.dwSize = sizeof(entry);
 
-	for (BOOL res = Process32First(snapshot, &entry); res; res = Process32Next(snapshot, &entry))
-	{
-		if (wcscmp(entry.szExeFile, file) == 0)
-		{
-			HANDLE process = OpenProcess(PROCESS_TERMINATE, FALSE, entry.th32ProcessID);
+    for (BOOL res = Process32First(snapshot, &entry); res; res = Process32Next(snapshot, &entry))
+    {
+        if (wcscmp(entry.szExeFile, file) == 0)
+        {
+            HANDLE process = OpenProcess(PROCESS_TERMINATE, FALSE, entry.th32ProcessID);
 
-			if (process != nullptr && entry.th32ProcessID != id)
-			{
-				TerminateProcess(process, 9);
-				CloseHandle(process);
-			}
-		}
-	}
+            if (process != nullptr && entry.th32ProcessID != id)
+            {
+                TerminateProcess(process, 9);
+                CloseHandle(process);
+            }
+        }
+    }
 
-	CloseHandle(snapshot);
+    CloseHandle(snapshot);
 }
 
 int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 {
-	KillOtherInstances();
-	CachePhysicalMonitor();
-	CacheVcpValues();
-	ReadLaunchParams();
+    KillOtherInstances();
+    CachePhysicalMonitor();
+    CacheVcpValues();
+    ReadLaunchParams();
 
-	HWND wnd = CreateMainWindow(instance);
-	CreateTrayIcon(wnd, LoadIcon(instance, MAKEINTRESOURCE(IDI_ICON1)));
-	RegisterPowerSettingNotification(wnd, &GUID_CONSOLE_DISPLAY_STATE, DEVICE_NOTIFY_WINDOW_HANDLE);
-	RegisterHotKey(wnd, 1000, MOD_ALT, VK_HOME);
+    HWND wnd = CreateMainWindow(instance);
+    CreateTrayIcon(wnd, LoadIcon(instance, MAKEINTRESOURCE(IDI_ICON1)));
+    RegisterPowerSettingNotification(wnd, &GUID_CONSOLE_DISPLAY_STATE, DEVICE_NOTIFY_WINDOW_HANDLE);
+    RegisterHotKey(wnd, 1000, MOD_ALT, VK_HOME); // Alt + Home
+    RegisterHotKey(wnd, 1001, MOD_ALT, VK_F8);   // Alt + F8
 
-	MSG msg;
+    MSG msg;
 
-	while (GetMessage(&msg, nullptr, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
-	return (int)msg.wParam;
+    return (int)msg.wParam;
 }
